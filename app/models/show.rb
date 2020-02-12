96cb41e26b1c64ab806d518f31736ffe7950fbbd
Daniel Reina <dannyflatiron@gmail.com>
2020-02-12 04:33:55 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    # binding.pry
+    actors.map do |character|
+      "#{character.first_name} #{character.last_name}"
+    end
+  end
+
+
 end
